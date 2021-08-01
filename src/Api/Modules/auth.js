0fdf7ea760ac893ab7c38b59dace8be365d518eb
MaduraPrasad <madura.prasad44@gmail.com>
2021-08-01 14:21:44 +0530

    'Updated'

--- src/Api/Modules/auth.js
+++ src/Api/Modules/auth.js
@@ -1,7 +1,7 @@
 import { api } from "@/Api/index";
 import { setToken, clearToken } from "../index";
 import notification from "@/ApiConstance/toast";
-
+//test
 export default {
   async login(payload) {
     return await api.post("/login", payload).then(res => {
