18e35eb86cc0eb956d1dc6fd94f6709df8091e5c
Pubudi123 <pubudi.aathmaja1998@gmail.com>
2021-08-02 19:31:59 +0530

    updated

--- src/router/auth_controller.js
+++ src/router/auth_controller.js
@@ -4,28 +4,6 @@ export default {
     router.beforeEach(async (to, _, next) => {
       await store.dispatch('autoLogin')
 
-<<<<<<< HEAD
-      const { isLogedIn } = store.getters;
-
-      if (to.meta.redirectIfLoggedIn && isLogedIn) {
-        next({
-          path: "/dashboard",
-          replace: true,
-        });
-      }
-
-      if (!to.meta.noAuth && !isLogedIn) {
-        next({
-          path: "/",
-          replace: true,
-        });
-      }
-
-      return next();
-    });
-  },
-};
-=======
       //   if (!canNavigate(to)) {
       //     if (!isLoggedIn) return next({ name: 'auth-login' })
       //     return next({ name: 'misc-not-authorized' })
@@ -61,4 +39,3 @@ export default {
     })
   },
 }
->>>>>>> dfc8461f8983b098141dff28ea3938c36f2c481a
