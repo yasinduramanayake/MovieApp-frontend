10ac225cc8947e8c41932cf9163f6aed45e57b39
Pubudi123 <pubudi.aathmaja1998@gmail.com>
2021-08-21 16:15:25 +0530

    updated

--- src/router/modules/error_route.js
+++ src/router/modules/error_route.js
@@ -2,7 +2,7 @@ export default [
   {
     path: '/error-404',
     name: 'error-404',
-    component: () => import('@/views/error/error404.vue'),
+    component: () => import('@/views/error/Error404.vue'),
     meta: {
       layout: 'full',
       noAuth: true,
