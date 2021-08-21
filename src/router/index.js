258d36da5a4df6642159efc73551e0b2d765f45d
MaduraPrasad <madura.prasad44@gmail.com>
2021-08-21 09:02:05 +0530

    Updated

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,10 @@ import moviesRoute from './modules/movies_route'
 import unitRoute from './modules/unit_route'
 import membersRoute from './modules/members_route'
 import registerRoute from './modules/register_route'
-<<<<<<< HEAD
 import movieRoute from './modules/main_movie'
-=======
 import paydetailsRoute from './modules/paydetails_route'
 import bookingRoute from './modules/booking_route'
 import theaterRoute from './modules/theater_route'
->>>>>>> yasindu
 
 Vue.use(VueRouter)
 
@@ -33,13 +30,10 @@ const router = new VueRouter({
     ...errorRoute,
     ...membersRoute,
     ...registerRoute,
-<<<<<<< HEAD
     ...movieRoute,
-=======
     ...paydetailsRoute,
     ...bookingRoute,
     ...theaterRoute,
->>>>>>> yasindu
   ],
 })
 
