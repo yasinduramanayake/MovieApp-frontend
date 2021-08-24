10f3dc0fdddb6c0a8666c86ba67ecc06acc1a856
Pubudi123 <pubudi.aathmaja1998@gmail.com>
2021-08-24 23:21:38 +0530

    updated

---
+++ src/views/components/NoresultFoundImageUser.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="padding-left:200px;padding-top:20px">
+    <b-card>
+      <h6 style="padding-left: 350px;">No entries found.</h6>
+    </b-card>
+  </div>
+</template>
+<script>
+import { BCard } from "bootstrap-vue";
+
+export default {
+  components: {
+    BCard,
+  },
+};
+</script>
