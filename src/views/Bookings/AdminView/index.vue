99f1fdb645bd8e9d8d3f1292b8feb2582535bc24
yasinduramanayake <yasinduramanayake123@gmail.com>
2021-09-04 17:26:27 +0530

    'updated'

--- src/views/Bookings/AdminView/index.vue
+++ src/views/Bookings/AdminView/index.vue
@@ -204,7 +204,7 @@ export default {
       this.currentPage = e;
       this.index();
     },
-    // comment
+    // comment for testing
     // fetch All data
     async index(reset = false, data = "") {
       if (reset) {
